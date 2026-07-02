3152fab32194b9aa148a15022c8fcfa0c0fdba2b
Elinor <elinor6almog@gmail.com>
2026-07-02 12:29:50 +0300

    Spotless

--- src/main/kotlin/frc/robot/subsystems/intake/Roller.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/Roller.kt
@@ -1,34 +1,31 @@
 package frc.robot.subsystems.intake
 
-import com.ctre.phoenix6.controls.VelocityVoltage
 import com.ctre.phoenix6.controls.VoltageOut
 import edu.wpi.first.units.measure.AngularVelocity
 import edu.wpi.first.units.measure.Voltage
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.button.Trigger
-import frc.robot.lib.Gains
 import frc.robot.lib.extensions.rps
 import frc.robot.lib.universal_motor.UniversalTalonFX
-import javax.xml.stream.events.Comment
 
 object Roller : SubsystemBase() {
-    private val motor = UniversalTalonFX(
-        config = MOTOR_CONFIG,
-        port = PORT,
-        simGains = SIM_GAINS
-    )
+    private val motor =
+        UniversalTalonFX(
+            config = MOTOR_CONFIG,
+            port = PORT,
+            simGains = SIM_GAINS
+        )
     private val setpoint: AngularVelocity = 0.rps
     private val voltageRequest = VoltageOut(0.0)
 
-    val isActive = Trigger {setpoint > 0.rps}
+    val isActive = Trigger { setpoint > 0.rps }
 
- fun setVoltage (voltage : Voltage) : Command = this.runOnce {
-     motor.setControl(voltageRequest.withOutput(voltage))
-    }
-    fun intake() : Command = setVoltage(INTAKE)
-    fun outTake() : Command = setVoltage(OUTTAKE)
-    fun stop() : Command = setVoltage(STOP)
+    fun setVoltage(voltage: Voltage): Command =
+        this.runOnce { motor.setControl(voltageRequest.withOutput(voltage)) }
+    fun intake(): Command = setVoltage(INTAKE)
+    fun outTake(): Command = setVoltage(OUTTAKE)
+    fun stop(): Command = setVoltage(STOP)
 
     override fun periodic() {
         motor.periodic()
--- src/main/kotlin/frc/robot/subsystems/intake/RollerConstants.kt
+++ src/main/kotlin/frc/robot/subsystems/intake/RollerConstants.kt
@@ -8,11 +8,11 @@ import com.ctre.phoenix6.signals.NeutralModeValue
 import frc.robot.lib.Gains
 import frc.robot.lib.extensions.volts
 
-val PORT=2
+val PORT = 2
 
-val REAL_GAINS = Gains(kP = 0.5 , kD = 0.1)
+val REAL_GAINS = Gains(kP = 0.5, kD = 0.1)
 
-val SIM_GAINS = Gains( kP = 0.5 , kD = 0.1)
+val SIM_GAINS = Gains(kP = 0.5, kD = 0.1)
 
 val GEAR_RATIO = 1.0
 
@@ -20,12 +20,14 @@ val INTAKE = 10.volts
 val OUTTAKE = -INTAKE
 val STOP = 0.volts
 
-val MOTOR_CONFIG = TalonFXConfiguration().apply {
-    MotorOutput =
-        MotorOutputConfigs().apply {
-            Inverted= InvertedValue.Clockwise_Positive
-            NeutralMode = NeutralModeValue.Coast
-        }
-    Slot0 = REAL_GAINS.toSlotConfig()
-    Feedback = FeedbackConfigs().apply { SensorToMechanismRatio = GEAR_RATIO}
-}
+val MOTOR_CONFIG =
+    TalonFXConfiguration().apply {
+        MotorOutput =
+            MotorOutputConfigs().apply {
+                Inverted = InvertedValue.Clockwise_Positive
+                NeutralMode = NeutralModeValue.Coast
+            }
+        Slot0 = REAL_GAINS.toSlotConfig()
+        Feedback =
+            FeedbackConfigs().apply { SensorToMechanismRatio = GEAR_RATIO }
+    }
